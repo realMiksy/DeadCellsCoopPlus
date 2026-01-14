d6e44a7a67db60fdac8eac95728b5568366431a4
ChiuYi <3360625360@qq.com>
2026-01-14 23:16:09 +0800

    Add Template

--- GhostCine/GhostDead/DeadBase.cs
+++ GhostCine/GhostDead/DeadBase.cs
@@ -1,10 +1,24 @@
+using dc.en;
+using Cooldown = CooldownHelper.Cooldown;
+
 namespace DeadCellsMultiplayerMod
 {
     public class DeadBase : dc.GameCinematic
     {
-        public DeadBase()
+        public DeadBase(Hero hero, KingSkin king)
         {
+            _DeadBase.EnterGhostDead(this, hero, king);
+        }
+        public override void update()
+        {
+            base.update();
+            if (!this.CanGohostCreate()) return;
+        }
 
+        public bool CanGohostCreate()
+        {
+            int k = Cooldown.Encode(Cooldown.Keys.KING_Create);
+            return ModEntry._companionKing.cd.fastCheck.exists(k);
         }
 
     }
--- GhostCine/GhostDead/_DeadBase.cs
+++ GhostCine/GhostDead/_DeadBase.cs
@@ -1,12 +1,60 @@
 using dc.en;
+using HaxeProxy.Runtime;
 
 namespace DeadCellsMultiplayerMod
 {
     public static class _DeadBase
     {
-        public static void EnterGhostDead(DeadBase cm, Hero hero, KingSkin king)
+        public static DeadBase? deadBase;
+        public static Hero? GetHero;
+        public static KingSkin? kingSkin;
+        public static void EnterGhostDead(DeadBase @base, Hero hero, KingSkin king)
         {
+            deadBase = @base;
+            GetHero = hero;
+            kingSkin = king;
+            var cm = @base.cm;
 
+
+            cm.__beginNewQueue();
+            cm.__add(AHlAchtion001(), 0, null);
+            cm.__add(AHlAchtion002(), 0, null);
+            cm.__add(AHlAchtion003(), 0, null);
+            cm.__add(AHlAchtion004(), 0, null);
+
+        }
+
+        private static HlAction AHlAchtion001()
+        {
+            HlAction hl = new HlAction(() =>
+            {
+
+            });
+            return hl;
+        }
+        private static HlAction AHlAchtion002()
+        {
+            HlAction hl = new HlAction(() =>
+            {
+
+            });
+            return hl;
+        }
+        private static HlAction AHlAchtion003()
+        {
+            HlAction hl = new HlAction(() =>
+            {
+
+            });
+            return hl;
+        }
+        private static HlAction AHlAchtion004()
+        {
+            HlAction hl = new HlAction(() =>
+            {
+
+            });
+            return hl;
         }
     }
 }
